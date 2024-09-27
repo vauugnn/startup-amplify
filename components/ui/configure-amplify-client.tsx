30a1ff9793dcaa8de490052b52edf2b546def1e0
vauugnn <vaughnrochederoda@gmail.com>
2024-09-27 20:53:42 +0800

    auth

--- components/ui/configure-amplify-client.tsx
+++ components/ui/configure-amplify-client.tsx
@@ -1,8 +1,8 @@
-// configure-amplify-client.tsx
 "use client";
 
-import outputs from "/home/vauugnn/projects/aws-workshop/startup-amplify/amplify_outputs.json";
+import outputs from "../../amplify_outputs.json";
 import { Amplify } from "aws-amplify";
+
 Amplify.configure(outputs, {
  ssr: true,
 });
